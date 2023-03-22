ae8c0cc4227a37227f8674ca6fd847bb525fbf01
Rohan Das <rohandas2468@gmail.com>
2023-03-23 01:08:42 +0530

    Update read_more.js

--- read_more.js
+++ read_more.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const magician = localStorage.getItem("magicians");
     const magicianObj = JSON.parse(magician);
 
-    var x = '<div class="home-video-details"><div class="home-heading"><h2 class="home-header">' + magicianObj["name"] + '</h2></div><div class="home-video-wrapper"><iframe width="560" height="315" src="https://www.youtube.com/embed/' + magicianObj["video_url"] + '/" frameborder="0" allowfullscreen></iframe></div></div><p class="home-caption">' + magicianObj["description"] + '</p>';
+    var x = '<div class="home-video-details"><div class="home-heading"><h2 class="home-header">' + magicianObj["name"] + '</h2></div><div class="home-video-wrapper"><iframe width="560" height="315" src="https://www.youtube.com/embed/' + magicianObj["video_url"].split('youtu.be/')[1] + '/" frameborder="0" allowfullscreen></iframe></div></div><p class="home-caption">' + magicianObj["description"] + '</p>';
 
     document.getElementById("magician").innerHTML = x;
 }
